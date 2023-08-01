8c079ed8d532672c592874aaa9615a60be7c00c8
InasAlSaab <sososaab618@gmail.com>
2023-08-01 18:09:38 +0300

    Inas

---
+++ lib/inas/inas_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class InaasView extends StatefulWidget {
+  const InaasView({super.key});
+
+  @override
+  State<InaasView> createState() => _InaasViewState();
+}
+
+class _InaasViewState extends State<InaasView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
