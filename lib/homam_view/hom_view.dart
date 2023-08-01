342d5cfde1c4483ef8c0306790a44c1994532520
HOMAM <homamr06@gmail.com>
2023-08-01 18:21:55 +0300

    Added a new from homam

---
+++ lib/homam_view/hom_view.dart
@@ -0,0 +1,14 @@
+
+class myview extends StatefulWidget {
+  const myview({Key? key}) : super(key: key);
+
+  @override
+  State<myview> createState() => _myviewState();
+}
+
+class _myviewState extends State<myview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/homam_view1/homam.dart
@@ -0,0 +1,8 @@
+class homam extends StatelessWidget {
+  const homam({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
