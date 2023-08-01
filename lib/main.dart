7ec304bf9f90b121e0c98a5e7013320b0f08f9ef
GhazalHazori <ghazalhazori2233@gmail.com>
2023-08-01 18:07:33 +0300

    first comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //ghazalhazaori
   runApp(const MyApp());
 }
 
@@ -9,10 +10,6 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-
-
-
-
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo new',
