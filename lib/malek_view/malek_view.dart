226ee19d294780940d40278623825aaf26ed3a00
Malek Alzein <malek.alzein@studentstore.se>
2023-08-01 18:19:58 +0300

    Add malek view

---
+++ lib/malek_view/malek_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MalekView extends StatelessWidget {
+  const MalekView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
